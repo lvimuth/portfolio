680e634c9897cd669ef8ed4e2b001826eca8a654
Lakshitha Vimuth <101648205+lvimuth@users.noreply.github.com>
2023-04-12 22:06:54 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let navbar = document.querySelector("nav-links");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
